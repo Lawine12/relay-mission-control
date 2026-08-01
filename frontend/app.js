59e611646424a469c3bd5ca6814fc10b97e00f1a
lawine12 <rkok12@gmail.com>
2026-08-01 11:39:59 +0200

    feat(frontend): add application shell

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,10 @@
 console.log('Relay Mission Control');
+import { RelayPanel } from "./relay-panel.js";
+
+window.addEventListener("DOMContentLoaded", () => {
+
+    const app = new RelayPanel(document.body);
+
+    app.render();
+
+});
